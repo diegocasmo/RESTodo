f760dbc6b20f43e14553b3a7abc633702b24d61d
Diego Castillo <diegogcm_@hotmail.com>
2014-11-09 15:12:07 -0500

    Added db.sql

---
+++ db.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 09, 2014 at 09:11 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `todo-api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todos`
+--
+
+CREATE TABLE IF NOT EXISTS `todos` (
+`id` int(10) unsigned NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `done` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `todos`
+--
+
+INSERT INTO `todos` (`id`, `title`, `done`, `created_at`, `updated_at`) VALUES
+(5, 'The same thing we do every night, Pinky - try to take over the world!', 0, '2014-11-10 02:00:32', '2014-11-10 02:04:13'),
+(6, 'They''re Pinky, They''re Pinky and the Brain Brain Brain Brain Brain!', 0, '2014-11-10 02:00:45', '2014-11-10 02:00:45'),
+(7, 'I am not a refrigerator.', 0, '2014-11-10 02:00:57', '2014-11-10 02:04:14');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `todos`
+--
+ALTER TABLE `todos`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `todos`
+--
+ALTER TABLE `todos`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
